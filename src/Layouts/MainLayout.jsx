38c0b7b3de511df628f268827c65ffd0d5860773
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-05-02 19:39:38 +0600

    add react back to top

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Shared/Navbar";
 import Footer from "../Components/Shared/Footer";
-
+import ScrollToTop from "react-scroll-to-top";
 const MainLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
+      <ScrollToTop className="p-1" smooth />
     </div>
   );
 };
