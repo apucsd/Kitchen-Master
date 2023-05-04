81c8821c682aa6c8160beafbee2c1d6d6f849deb
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-05-04 11:57:24 +0600

    fix scroll in UserDetails page

--- src/Components/Pages/UserDetails.jsx
+++ src/Components/Pages/UserDetails.jsx
@@ -18,15 +18,15 @@ const UserDetails = () => {
 
   //   console.log(user);
   return (
-    <div className="md:flex gap-1 m-8">
+    <div className="md:flex gap-1 ">
       <div>
         <img
-          className="w-80 border-2 rounded border-slate-400"
+          className="w-80 mx-auto border-2 rounded border-slate-400"
           src={user.photoURL}
           alt=""
         />
       </div>
-      <div className="bg-white p-3 shadow-sm rounded-sm ">
+      <div className="bg-white m-3 shadow-sm rounded-sm ">
         <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
           <span className="text-green-500">
             <svg
@@ -63,15 +63,15 @@ const UserDetails = () => {
                 {user.phoneNumber ? phoneNumber : "+8801998001001"}
               </div>
             </div>
-            <div className="grid grid-cols-2">
+            <div className="grid md:grid-cols-2">
               <div className="px-4 py-2 font-semibold">Current Address</div>
               <div className="px-4 py-2">Narsingdi,Dhaka,Bangladesh</div>
             </div>
-            <div className="grid grid-cols-2">
+            <div className="grid md:grid-cols-2">
               <div className="px-4 py-2 font-semibold">Permanent Address</div>
               <div className="px-4 py-2">Narsingdi,Dhaka,Bangladesh</div>
             </div>
-            <div className="grid grid-cols-2">
+            <div className="grid md:grid-cols-2">
               <div className="px-4 py-2 font-semibold">Email.</div>
               <div className="px-4 py-2">
                 <a className="text-blue-800" href="mailto:jane@example.com">
